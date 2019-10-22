d0d03b12cfb9a2e64929444e4bfcd3a86a7d3fbf
Malcolm Meyer <malcolmtmeyer@gmail.com>
2019-10-22 19:28:39 -0400

    Update layerControl.js

--- layerControl.js
+++ layerControl.js
@@ -2,9 +2,9 @@ function layerControl() { }
 
 layerControl.prototype.onAdd = function(map) {
   this._map = map;
-  this._container = document.createElement(‘div’);
-  this._container.className = ‘mapboxgl-ctrl’;
-  this._container.textContent = ‘Hello, world’;
+  this._container = document.createElement('div');
+  this._container.className = 'mapboxgl-ctr';
+  this._container.textContent = 'Hello, world';
   return this._container;
 };
 
